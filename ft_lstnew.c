36d3d709dfd40ced319f15c2cda0a8236d591f8f
Jerome CHARDIN <jchardin@e2r5p7.42.fr>
2018-12-04 08:11:24 +0100

    save

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: jchardin <jerome.chardin@outlook.co>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/02 15:09:16 by jchardin          #+#    #+#             */
-/*   Updated: 2018/12/03 15:53:15 by jchardin         ###   ########.fr       */
+/*   Updated: 2018/12/04 08:10:25 by jchardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,7 @@ t_list		*ft_lstnew(void const *content, size_t content_size)
 {
 	t_list	*new_node;
 
-	new_node = (t_list*)malloc(sizeof(t_list));
-	if (!new_node)
+	if (!(new_node = (t_list*)malloc(sizeof(t_list))))
 		return (0);
 	if (content == NULL)
 	{
@@ -27,7 +26,8 @@ t_list		*ft_lstnew(void const *content, size_t content_size)
 	else
 	{
 		new_node->content_size = content_size;
-		new_node->content = (char*)malloc(sizeof(char) * content_size);
+		if (!(new_node->content = (char*)malloc(sizeof(char) * content_size)))
+			return (0);
 		ft_memcpy(new_node->content, content, content_size);
 	}
 	new_node->next = NULL;
